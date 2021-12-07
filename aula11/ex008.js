1db1f521dadb0bf25fe10b035471abfa555f8d69
Guilherme Kudlawiec <guilhermejoaokudlawiec@gmail.com>
2021-12-07 09:30:28 -0300

    Condições

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -2,11 +2,3 @@ var jogo = 'God of War'
 if (jogo != 'Fortnite') {
     console.log('Achei que era Fortnite...')
 }
-
-var nome = 'Kratos'
-if (nome == 'Guilherme') {
-    console.log(`O nome armazenado na variavel é Guilherme, né ${nome}?`)
-}
-else {
-    console.log(`O nome não é Guilherme, o nome dele é: ${nome}`)
-}
