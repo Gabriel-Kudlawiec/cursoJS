ff26a980ae978faf5f956593897138a6b75ea969
Guilherme Kudlawiec <guilhermejoaokudlawiec@gmail.com>
2021-12-15 09:49:08 -0300

    Enquanto

--- aula12-ex/ex014/script.js
+++ aula12-ex/ex014/script.js
@@ -3,11 +3,12 @@ function carregar() {
     var hora = new Date()
     var hora_atual = hora.getHours()
     var msg = window.document.getElementById('msg')
-    if (hora_atual < 12) {
+    hora_atual = 19
+    if (hora_atual <= 12) {
         img.src = "fotomanha.jpg"
         msg.innerHTML = 'BOM DIA!'
         window.document.body.style.background = '#e2cd9f'
-    } else if (hora_atual > 12 && hora_atual < 18) {
+    } else if (hora_atual > 12 && hora_atual <= 18) {
         img.src = "fototarde.jpg"
         msg.innerHTML = 'BOA TARDE!'
         window.document.body.style.background = '#b9846f'
---
+++ aula13/enquanto.js
@@ -0,0 +1,18 @@
+var contador = 1
+while (contador <= 10) {
+    console.log(`Grito de é campeão, ${contador}.`)
+    contador++
+}
+
+/*
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+console.log(`É campeão!`)
+*/
